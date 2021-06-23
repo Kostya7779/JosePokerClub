d85eae53a8d6d7c2db943ed05e8baea53fcd1768
Kostya7779 <86355643+Kostya7779@users.noreply.github.com>
2021-06-23 17:57:23 +0600

    Add files via upload

---
+++ App/JosePokerApp/JosePokerApp/MainForm.Designer.cs
@@ -0,0 +1,303 @@
+﻿
+namespace JosePokerApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1-й стол";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2-й стол";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3-й стол";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4-й стол";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "5-й стол";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "6-й стол";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "7-й стол";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 247);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "8-й стол";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 283);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "9-й стол";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 315);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(59, 15);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "10-й стол";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 45);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Рассчитать мин. кол-во ходов фишек";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 6);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 23);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(71, 36);
+            this.textBox2.MaxLength = 1;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(61, 23);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 69);
+            this.textBox3.MaxLength = 1;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(61, 23);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(71, 99);
+            this.textBox4.MaxLength = 1;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(61, 23);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(71, 131);
+            this.textBox5.MaxLength = 1;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(61, 23);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(71, 166);
+            this.textBox6.MaxLength = 1;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(61, 23);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(71, 205);
+            this.textBox7.MaxLength = 1;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(61, 23);
+            this.textBox7.TabIndex = 17;
+            this.textBox7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(71, 239);
+            this.textBox8.MaxLength = 1;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(61, 23);
+            this.textBox8.TabIndex = 18;
+            this.textBox8.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(71, 280);
+            this.textBox9.MaxLength = 1;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(61, 23);
+            this.textBox9.TabIndex = 19;
+            this.textBox9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(71, 312);
+            this.textBox10.MaxLength = 1;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(61, 23);
+            this.textBox10.TabIndex = 20;
+            this.textBox10.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 390);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "JosePokerClub";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ App/JosePokerApp/JosePokerApp/MainForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JosePokerApp
+{
+    public partial class MainForm : Form
+    {
+        public int count = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public void Rash()
+        {
+            int[] nums = new int[10] { Convert.ToInt32(textBox1.Text),
+            Convert.ToInt32(textBox2.Text),
+            Convert.ToInt32(textBox3.Text),
+            Convert.ToInt32(textBox4.Text),
+            Convert.ToInt32(textBox5.Text),
+            Convert.ToInt32(textBox6.Text),
+            Convert.ToInt32(textBox7.Text),
+            Convert.ToInt32(textBox8.Text),
+            Convert.ToInt32(textBox9.Text),
+            Convert.ToInt32(textBox10.Text)};
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[0] > nums[1])
+                {
+                  nums[0] = nums[0] - 1;
+                  nums[1] = nums[1] + 1;
+                  count++;
+                } if (nums[0] < nums[1])
+                {
+                    nums[1] = nums[1] - 1;
+                    nums[0] = nums[0] + 1;
+                    count++;
+                } 
+                if (nums[1] > nums [2])
+                {
+                    nums[1] = nums[1] - 1;
+                    nums[2] = nums[2] + 1;
+                    count++;
+                } if (nums[1] < nums[2])
+                {
+                    nums[2] = nums[2] - 1;
+                    nums[1] = nums[1] + 1;
+                    count++;
+                }
+                if (nums[2] > nums[3])
+                {
+                    nums[2] = nums[2] - 1;
+                    nums[3] = nums[3] + 1;
+                    count++;
+                }
+                if (nums[2] < nums[3])
+                {
+                    nums[3] = nums[3] - 1;
+                    nums[2] = nums[2] + 1;
+                    count++;
+                }
+                if (nums[3] > nums[4])
+                {
+                    nums[3] = nums[3] - 1;
+                    nums[4] = nums[4] + 1;
+                    count++;
+                }
+                if (nums[3] < nums[4])
+                {
+                    nums[4] = nums[4] - 1;
+                    nums[3] = nums[3] + 1;
+                    count++;
+                }
+                if (nums[4] > nums[5])
+                {
+                    nums[4] = nums[4] - 1;
+                    nums[5] = nums[5] + 1;
+                    count++;
+                }
+                if (nums[4] < nums[5])
+                {
+                    nums[5] = nums[5] - 1;
+                    nums[4] = nums[4] + 1;
+                    count++;
+                }
+                if (nums[5] > nums[6])
+                {
+                    nums[5] = nums[5] - 1;
+                    nums[6] = nums[6] + 1;
+                    count++;
+                }
+                if (nums[5] < nums[6])
+                {
+                    nums[6] = nums[6] - 1;
+                    nums[5] = nums[5] + 1;
+                    count++;
+                }
+                if (nums[6] > nums[7])
+                {
+                    nums[6] = nums[6] - 1;
+                    nums[7] = nums[7] + 1;
+                    count++;
+                }
+                if (nums[6] < nums[7])
+                {
+                    nums[7] = nums[7] - 1;
+                    nums[6] = nums[6] + 1;
+                    count++;
+                }
+                if (nums[7] > nums[8])
+                {
+                    nums[7] = nums[7] - 1;
+                    nums[8] = nums[8] + 1;
+                    count++;
+                }
+                if (nums[7] < nums[8])
+                {
+                    nums[8] = nums[8] - 1;
+                    nums[7] = nums[7] + 1;
+                    count++;
+                }
+                if (nums[8] > nums[9])
+                {
+                    nums[8] = nums[8] - 1;
+                    nums[9] = nums[9] + 1;
+                    count++;
+                }
+                if (nums[8] < nums[9])
+                {
+                    nums[9] = nums[9] - 1;
+                    nums[8] = nums[8] + 1;
+                    count++;
+                }
+                if (nums[i] == nums[i])
+                {
+                   MessageBox.Show("Количество ходов фишек равно =" + count, "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox1.Text != ""  && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "" && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "" && textBox10.Text != "")
+            {
+                Rash();
+            } else { MessageBox.Show("У каждого столика не может быть пустых значений, есоли вы хотите чтоб у игрока не было фишек, то укажите «0».", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)) return;
+            else
+                e.Handled = true;
+        }
+    }
+}
